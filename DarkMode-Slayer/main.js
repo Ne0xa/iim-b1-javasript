1cdfd6f8ce4704517794b23eeedee06499bc9ab9
Ne0xa <finebenjamin.e@gmail.com>
2025-11-14 09:54:54 +0100

    add burger menu

--- DarkMode-Slayer/main.js
+++ DarkMode-Slayer/main.js
@@ -22,6 +22,33 @@ toggleBtn.addEventListener('click', () => {
   }
 });
 
+// burger menu
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const burger= document.querySelector(".burger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+burger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+
 // This is an exemple of my lesson
 
 // let title = document.getElementById("title");
